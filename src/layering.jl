da3da9f5542bdea6cde46006ce22211fd14a453f
Thomas Christensen <tchr@mit.edu>
2024-06-03 09:23:17 +0200

    Update src/layering.jl
    Co-authored-by: Frames White <oxinabox@ucc.asn.au>

--- src/layering.jl
+++ src/layering.jl
@@ -3,7 +3,10 @@
 function layer_by_longest_path_to_source(graph, force_layer)
     dists = longest_paths(graph, sources(graph))
     force_layers!(graph, dists, force_layer)
-    layer_groups = [findall(==(d), dists) for d in 1:maximum(dists)]
+    layer_groups = [Int[] for d in 1:maximum(dists)]
+    for (ii, d) in enumerate()
+        push!(layer_groups[d], ii)
+    end
 
     return layer_groups
 end
